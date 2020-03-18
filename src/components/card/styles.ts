ad4e061e89c7d5af0da13e38441cefa17be3a5b4
josemak25 <amakirij@gmail.com>
2020-03-18 11:06:35 +0100

    add fix to card component min width and height and implemented tochacble opacity on card

--- src/components/card/styles.ts
+++ src/components/card/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.TouchableOpacity`
-  min-width: 200px;
-  min-height: 200px;
+  width: 200px;
+  height: 200px;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.BD_DARK_COLOR};
 `;
