06e9721405a4c478e538b6bf412a87410449970f
albertakpos <albertakpos@gmail.com>
2020-03-19 17:54:36 +0100

    add filled cart page

--- src/screens/cart/filled-cart.tsx
+++ src/screens/cart/filled-cart.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { FlatList } from 'react-native';
 import Button from '../../components/button';
 import { NavigationInterface } from '../types';
 import { useThemeContext } from '../../theme';
@@ -6,8 +7,19 @@ import boxShadow from '../../utils/boxShadows';
 import { ProductInterface } from '../../store/product/types';
 import TrollerCartIcon from '../../../assets/icons/troller-icon';
 
-import { Container, trollerCartStyles, CartItemNumber } from './styles';
+import {
+  Container,
+  trollerCartStyles,
+  CartItemNumber,
+  FilledCartHolder,
+  FilledCartItem,
+  FilledCartBtnContainer,
+  FilledCardBtnOne,
+  FilledCartBtnTwo,
+  ProductsContainer
+} from './styles';
 import Card from '../../components/card';
+import CartItem from './CartItem';
 
 interface CartProp extends NavigationInterface {
   testID?: string;
@@ -16,6 +28,7 @@ interface CartProp extends NavigationInterface {
 
 export default function FilledCart(props: CartProp) {
   const { colors } = useThemeContext();
+  const { cart } = props;
 
   return (
     <Container style={{ width: '100%' }}>
@@ -43,6 +56,54 @@ export default function FilledCart(props: CartProp) {
           />
         </Fragment>
       </Card>
+
+      <FilledCartHolder>
+        <ProductsContainer>
+          <FlatList
+            data={cart}
+            renderItem={({ item }) => (
+              <CartItem product={item} navigation={props.navigation} />
+            )}
+          />
+        </ProductsContainer>
+        <FilledCartBtnContainer>
+          <Card
+            style={[
+              {
+                ...FilledCardBtnOne,
+                backgroundColor: '#FBFBFC'
+              },
+              boxShadow({
+                elevation: 2,
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+                shadowRadius: 2,
+                shadowOpacity: 0.1,
+                height: 2
+              })
+            ]}
+          >
+            <FilledCartItem>Total: </FilledCartItem>
+          </Card>
+
+          <Card
+            style={[
+              { ...FilledCartBtnTwo },
+
+              boxShadow({
+                elevation: 2,
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+                shadowRadius: 2,
+                shadowOpacity: 0.1,
+                height: 2
+              })
+            ]}
+          >
+            <FilledCartItem style={{ color: 'white' }}>
+              CHECK OUT
+            </FilledCartItem>
+          </Card>
+        </FilledCartBtnContainer>
+      </FilledCartHolder>
     </Container>
   );
 }
