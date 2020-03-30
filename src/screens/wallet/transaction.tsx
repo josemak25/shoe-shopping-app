f0c7cf9565bc5e1430267f6110269a49465ea0a2
josemak25 <amakirij@gmail.com>
2020-03-30 08:57:27 +0100

    add transaction componet for wallet screen

---
+++ src/screens/wallet/transaction.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import WalletIcon, { FundWalletIcon } from '../../../assets/icons/wallet';
+import { useThemeContext } from '../../theme';
+import CartIcon from '../../../assets/icons/cart-icon';
+
+import {
+  TransactionContainer,
+  TransactionTypeIconContainer,
+  TransactionDetailsContainer,
+  TransactionTypeContainer,
+  TransactionPrice,
+  TransactionWalletAddress,
+  TransactionType
+} from './styles';
+
+interface TransactionProp {
+  testID?: string;
+  transactionType: string;
+  walletAddress: string;
+  amount: string;
+}
+
+export default function Transaction(props: TransactionProp) {
+  const { colors } = useThemeContext();
+  const { transactionType, walletAddress, amount } = props;
+
+  return (
+    <TransactionContainer>
+      <TransactionDetailsContainer>
+        <TransactionTypeIconContainer>
+          {transactionType === 'fund wallet' && (
+            <WalletIcon
+              fillColor={colors.ACTIVE_TAB_ICON_COLOR}
+              width="50%"
+              height="50%"
+              isFocused
+            />
+          )}
+
+          {transactionType === 'shopping' && (
+            <CartIcon
+              fillColor={colors.ACTIVE_TAB_ICON_COLOR}
+              isFocused
+              width="50%"
+              height="50%"
+            />
+          )}
+
+          {transactionType === 'fund transfer' && (
+            <FundWalletIcon fillColor={colors.ACTIVE_TAB_ICON_COLOR} />
+          )}
+        </TransactionTypeIconContainer>
+        <TransactionTypeContainer>
+          <TransactionType>{transactionType}</TransactionType>
+          <TransactionWalletAddress>{walletAddress}</TransactionWalletAddress>
+        </TransactionTypeContainer>
+      </TransactionDetailsContainer>
+      <TransactionPrice>{amount}</TransactionPrice>
+    </TransactionContainer>
+  );
+}
